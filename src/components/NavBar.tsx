e813b94f3497a58e5dee5d60273f685def15110f
mayrang <pkss0626@naver.com>
2023-05-02 14:31:32 +0900

    project 생성, NavBar 디자인 초안

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav className="fixed w-full h-14  bg-white   shadow-sm">
+      <div className="flex h-full w-full items-center px-5 justify-between border border-b">
+        <Link href="/" className="font-bold text-2xl">
+          Next Blog
+        </Link>
+        <div className="flex items-center">
+          <Link className="text-lg mr-4" href="/">
+            {" "}
+            Home
+          </Link>
+          <Link className="text-lg mr-4" href="/about">
+            About
+          </Link>
+          <Link className="text-lg mr-4" href="/posts">
+            Post
+          </Link>
+          <Link className="text-lg" href="/contact">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
